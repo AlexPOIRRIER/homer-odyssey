8ecc305f12b38ff9fc39bd165962387beb04d26b
alexpoirrier <alex.poirrier@gmail.com>
2020-12-14 10:39:01 +0100

    ADD inputs and submit

--- front/src/components/SignUp.jsx
+++ front/src/components/SignUp.jsx
@@ -1,16 +1,62 @@
 import React, { useState } from "react";
 
 const SignUp = () => {
-  const [email, setEmail] = useState("");
-  
-  const updateEmailField = (e) => {
-    setEmail(e.target.value);
+  const [user, setUser] = useState({
+    email: "test@test.com",
+    password: "azerty",
+    name: "test",
+    lastname: "TEST",
+  });
+
+  const updateUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
   };
 
   return (
     <>
-      <h1>{email}</h1>
-      <input type="email" name="email" onChange={updateEmailField} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          E-mail
+          <input type="email" name="email" id="email" onChange={updateUser} />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input type="text" name="name" id="name" onChange={updateUser} />
+        </label>
+        <label htmlFor="lastname">
+          Lastname
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            onChange={updateUser}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={updateUser}
+          />
+        </label>
+        <label htmlFor="passwordBis">
+          Confirm Password
+          <input
+            type="password"
+            name="passwordBis"
+            id="passwordBis"
+            onChange={updateUser}
+          />
+        </label>
+        <input type="submit" />
+      </form>
     </>
   );
 };
